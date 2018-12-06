96f38c757481ad063e7d72dc753ab1c7fb4f4fd6
Aaron Aares Taylor <31523703+aares@users.noreply.github.com>
2018-12-06 11:35:16 -0500

    Add files via upload

---
+++ TextSearch.py
@@ -0,0 +1,38 @@
+#Aaron Taylor CSCE355 Project
+#Text Searcher
+import sys
+
+def main():
+    #Initializing
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'         #set alphabet
+    args = sys.argv                                 #receive args input
+    inputStringFile = open('str1.txt', 'r')            #input[1] from cmdLine = strX.txt
+    inputString = inputStringFile.readline().rstrip('\n')#inputString = firstLine of strX
+    strLen = len(inputString)                       #Length for iterator and print
+
+    #Transition Table
+    columnList = []                                 #List
+    for i in range(strLen):                         #iterate thru inputString length
+        rowList = []
+        currentToken = inputString[i]               #get currentToken from inputString
+        for j in range(len(alphabet)):              #iterate thru alphabet length
+            rowList.append(str(0))                  #populate blank row [0,0,,,,0], where len rowList = alphabet
+        for j in range(len(alphabet)):              #iterate thru alphabet tranTable[j]
+            if (alphabet[j] == currentToken):       #if currentToken == value on tranTable[j]
+                rowList[j] = i+1                    #set tranTableRow[j] to i+1 (value of the state)
+            if (i == strLen):
+                rowList[j] = i
+        columnList.append(rowList)                  #push row into tranTable until at the end of inputString
+
+    #Print Output
+    print("Number of states: " + str(strLen+1))
+    print("Accepting states: " + str(strLen))
+    print("Alphabet: " + alphabet)
+    for i in range(len(columnList)):
+        print(columnList[i])
+
+
+
+    
+    
+
