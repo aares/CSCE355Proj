2a71e0c8d9ef296866026039f4966faf20be971d
Aaron Aares Taylor <31523703+aares@users.noreply.github.com>
2018-12-06 17:03:25 -0500

    Add files via upload

--- TextSearch.py
+++ TextSearch.py
@@ -6,7 +6,7 @@ def main():
     #Initializing
     alphabet = 'abcdefghijklmnopqrstuvwxyz'         #set alphabet
     args = sys.argv                                 #receive args input
-    inputStringFile = open('str1.txt', 'r')            #input[1] from cmdLine = strX.txt
+    inputStringFile = open('str1.txt', 'r')         #input[1] from cmdLine = strX.txt
     inputString = inputStringFile.readline().rstrip('\n')#inputString = firstLine of strX
     strLen = len(inputString)                       #Length for iterator and print
 
@@ -14,15 +14,23 @@ def main():
     columnList = []                                 #List
     for i in range(strLen):                         #iterate thru inputString length
         rowList = []
+        repeatArray = []
+        repeatIterator = 0
         currentToken = inputString[i]               #get currentToken from inputString
+        
         for j in range(len(alphabet)):              #iterate thru alphabet length
-            rowList.append(str(0))                  #populate blank row [0,0,,,,0], where len rowList = alphabet
+            rowList.append(0)                       #populate blank row [0,0,,,,0], where len rowList = alphabet
+
         for j in range(len(alphabet)):              #iterate thru alphabet tranTable[j]
             if (alphabet[j] == currentToken):       #if currentToken == value on tranTable[j]
                 rowList[j] = i+1                    #set tranTableRow[j] to i+1 (value of the state)
-            if (i == strLen):
-                rowList[j] = i
-        columnList.append(rowList)                  #push row into tranTable until at the end of inputString
+        columnList.append(rowList)
+
+    #appends final row of Transition Table   
+    finalRowList = []   
+    for k in range(len(alphabet)):
+       finalRowList.append(strLen)
+    columnList.append(finalRowList)
 
     #Print Output
     print("Number of states: " + str(strLen+1))
@@ -31,8 +39,4 @@ def main():
     for i in range(len(columnList)):
         print(columnList[i])
 
-
-
     
-    
-
