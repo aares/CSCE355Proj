47fdd3e38088d64bc93e9c663da89ad062c6e085
Aaron Aares Taylor <31523703+aares@users.noreply.github.com>
2018-12-06 17:51:23 -0500

    Add files via upload

--- TextSearch.py
+++ TextSearch.py
@@ -2,41 +2,43 @@
 #Text Searcher
 import sys
 
-def main():
-    #Initializing
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'         #set alphabet
-    args = sys.argv                                 #receive args input
-    inputStringFile = open('str1.txt', 'r')         #input[1] from cmdLine = strX.txt
-    inputString = inputStringFile.readline().rstrip('\n')#inputString = firstLine of strX
-    strLen = len(inputString)                       #Length for iterator and print
+#Initializing
+alphabet = 'abcdefghijklmnopqrstuvwxyz'         #set alphabet
+args = sys.argv                                 #receive args input
+inputStringFile = open(args[1], 'r')            #input[1] from cmdLine = strX.txt
+inputString = inputStringFile.readline().rstrip('\n')#inputString = firstLine of strX
+strLen = len(inputString)                       #Length for iterator and print
 
-    #Transition Table
-    columnList = []                                 #List
-    for i in range(strLen):                         #iterate thru inputString length
-        rowList = []
-        repeatArray = []
-        repeatIterator = 0
-        currentToken = inputString[i]               #get currentToken from inputString
+#Transition Table
+columnList = []                                 #List
+for i in range(strLen):                         #iterate thru inputString length
+    rowList = []
+    currentToken = inputString[i]               #get currentToken from inputString
+    for j in range(len(alphabet)):              #iterate thru alphabet length
+        rowList.append(0)                       #populate blank row [0,0,,,,0], where len rowList = alphabet
+
+    for j in range(len(alphabet)):              #iterate thru alphabet tranTable[j]
+        if (alphabet[j] == currentToken):       #if currentToken == value on tranTable[j]
+            rowList[j] = i+1                    #set tranTableRow[j] to i+1 (value of the state)
+    columnList.append(rowList)    
+#appends final row of Transition Table   
+finalRowList = []   
+for k in range(len(alphabet)):
+   finalRowList.append(strLen)
+columnList.append(finalRowList)
+
+
+#create Comparison Array
+#if (currentToken == inputString[0]): #if currentToken = firstToken
+    #while
+
+#Print Output
+print("Number of states: " + str(strLen+1))
+print("Accepting states: " + str(strLen))
+print("Alphabet: " + alphabet)
+for i in range(len(columnList)):
+    print(columnList[i])
+
         
-        for j in range(len(alphabet)):              #iterate thru alphabet length
-            rowList.append(0)                       #populate blank row [0,0,,,,0], where len rowList = alphabet
-
-        for j in range(len(alphabet)):              #iterate thru alphabet tranTable[j]
-            if (alphabet[j] == currentToken):       #if currentToken == value on tranTable[j]
-                rowList[j] = i+1                    #set tranTableRow[j] to i+1 (value of the state)
-        columnList.append(rowList)
-
-    #appends final row of Transition Table   
-    finalRowList = []   
-    for k in range(len(alphabet)):
-       finalRowList.append(strLen)
-    columnList.append(finalRowList)
-
-    #Print Output
-    print("Number of states: " + str(strLen+1))
-    print("Accepting states: " + str(strLen))
-    print("Alphabet: " + alphabet)
-    for i in range(len(columnList)):
-        print(columnList[i])
 
     
